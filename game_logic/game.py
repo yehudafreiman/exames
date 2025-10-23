a713cacb2ed20b9e77455ee628579892132ee84d
Yehuda Freiman <yehfre@icloud.com>
2025-10-23 10:46:58 +0300

    add func

---
+++ game_logic/game.py
@@ -0,0 +1,12 @@
+def create_player(name:str):
+    return {}
+
+def init_game():
+    return {}
+
+def play_round(p1:dict, p2:dict):
+    return {}
+
+
+
+
---
+++ utils/deck.py
@@ -0,0 +1,12 @@
+def create_card(rank:str,suite:str):
+    return {}
+
+def compare_cards(p1_card:dict, p2_card:dict):
+    return ''
+
+def create_deck():
+    return [{}]
+
+def shuffle(deck:list[dict]):
+    return [{}]
+
